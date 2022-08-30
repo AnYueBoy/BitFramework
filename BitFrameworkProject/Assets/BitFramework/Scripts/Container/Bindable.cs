6b8ef58c9d7adf35228e5c73ce9f5c1cce8c3d2c
anyue <1183767577@qq.com>
2022-08-30 18:38:51 +0800

    临时提交

---
+++ BitFrameworkProject/Assets/BitFramework/Scripts/Container/Bindable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BitFramework.Container
+{
+    public abstract class Bindable : IBindable
+    {
+        private readonly Container container;
+
+        // 上下文
+        private Dictionary<string, string> contextual;
+
+        // 上下文闭包
+        private Dictionary<string, Func<object>> contextualClosure;
+        private bool isDestroy;
+
+        protected Bindable(Container container, string service)
+        {
+            this.container = container;
+            Service = service;
+            isDestroy = false;
+        }
+
+
+        public string Service { get; }
+
+        public IContainer Container => container;
+
+        public void Unbind()
+        {
+            isDestroy = true;
+            ReleaseBind();
+        }
+
+        /// <inheritdoc cref="Unbind"/>
+        protected abstract void ReleaseBind();
+    }
+}
