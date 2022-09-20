6950c2ff25cce05ddefff65c1c695927f7b1ce8a
anyue <1183767577@qq.com>
2022-09-20 18:18:20 +0800

    添加BindData中OnResolving的扩展方法

--- BitFrameworkProject/Assets/BitFramework/Scripts/Container/Extension/BindDataExtension.cs
+++ BitFrameworkProject/Assets/BitFramework/Scripts/Container/Extension/BindDataExtension.cs
@@ -1,7 +1,53 @@
-﻿namespace BitFramework.Container
+﻿using System;
+using BitFramework.Util;
+
+namespace BitFramework.Container
 {
-    public class BindDataExtension
+    public static class BindDataExtension
     {
-        
+        public static IBindData Alias<T>(this IBindData bindData)
+        {
+            return bindData.Alias(bindData.Container.TypeConvertToService(typeof(T)));
+        }
+
+        #region OnResolving
+
+        public static IBindData OnResolving(this IBindData bindData, Action closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((_, instance) => { closure(); });
+        }
+
+        public static IBindData OnResolving(this IBindData bindData, Action<object> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((_, instance) => { closure(instance); });
+        }
+
+        public static IBindData OnResolving<T>(this IBindData bindData, Action<T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((_, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure((T)instance);
+                }
+            });
+        }
+
+        public static IBindData OnResolving<T>(this IBindData bindData, Action<IBindData, T> closure)
+        {
+            Guard.Requires<ArgumentNullException>(closure != null);
+            return bindData.OnResolving((bind, instance) =>
+            {
+                if (instance is T)
+                {
+                    closure(bind, (T)instance);
+                }
+            });
+        }
+
+        #endregion
     }
 }
