eb3a2ea1cbdbda8a7ce32f1c309a89f0965599ba
anyue <1183767577@qq.com>
2022-09-23 14:23:50 +0800

    添加框架事件

---
+++ BitFrameworkProject/Assets/BitFramework/Scripts/BitApplication.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BitFramework.Core;
+using BitFramework.EventDispatcher;
+using IServiceProvider = BitFramework.Core.IServiceProvider;
+
+namespace BitFramework.Scripts
+{
+    public class BitApplication : Container.Container, IApplication
+    {
+        private static string version;
+        private readonly IList<IServiceProvider> loadedProviders;
+        private readonly int mainThreadId;
+        private readonly IDictionary<Type, string> dispatchMapping;
+        private bool bootstrapped;
+        private bool inited;
+        private bool registering;
+        private long incrementId;
+        private DebugLevel debugLevel;
+        private IEventDispatcher dispatcher;
+        
+        public bool IsMainThread { get; }
+        public DebugLevel DebugLevel { get; set; }
+
+        public IEventDispatcher GetDispatcher()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Register(IServiceProvider provider, bool force = false)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsRegistered(IServiceProvider provider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public long GetRuntimeId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Terminate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BitFrameworkProject/Assets/BitFramework/Scripts/Core/EventArgs/BitApplicationEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BitFramework.Core
+{
+    /// <summary>
+    /// 表示应用程序事件
+    /// </summary>
+    public class BitApplicationEventArgs : EventArgs
+    {
+        public IApplication BitApplication { get; }
+
+        public BitApplicationEventArgs(IApplication bitApplication)
+        {
+            BitApplication = bitApplication;
+        }
+    }
+}
