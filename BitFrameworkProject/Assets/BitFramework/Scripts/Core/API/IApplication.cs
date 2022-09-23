b2297d773b0df0c24be5ec901ab6cae5e70cd8a0
anyue <1183767577@qq.com>
2022-09-23 13:45:40 +0800

    添加框架核心的api

---
+++ BitFrameworkProject/Assets/BitFramework/Scripts/Core/API/IApplication.cs
@@ -0,0 +1,12 @@
+﻿using BitFramework.Container;
+
+namespace BitFramework.Core
+{
+    public interface IApplication : IContainer
+    {
+        /// <summary>
+        /// 该值指示在主线程上
+        /// </summary>
+        bool IsMainThread { get; }
+    }
+}
