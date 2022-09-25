f5bbccc6dacce067d24ddd48b806976bb53dc9d8
anyue <1183767577@qq.com>
2022-09-25 22:08:05 +0800

    添加框架运行时的部分逻辑

---
+++ BitFrameworkProject/Assets/BitFramework/Scripts/Runtime/UnityApplication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BitFramework.Container;
+using BitFramework.Exception;
+using BitFramework.Scripts;
+using UnityEngine;
+using IServiceProvider = BitFramework.Core.IServiceProvider;
+
+namespace BitFramework.Runtime
+{
+    public class UnityApplication : BitApplication
+    {
+        /// <summary>
+        /// 初始化UnityApplication的新实例
+        /// </summary>
+        /// <param name="behaviour">驱动器</param>
+        public UnityApplication(MonoBehaviour behaviour)
+        {
+            if (behaviour == null)
+            {
+                return;
+            }
+
+            this.Singleton<MonoBehaviour>(() => behaviour).Alias<Component>();
+        }
+
+        public override void Register(IServiceProvider provider, bool force = false)
+        {
+            var component = provider as Component;
+            // 从MonoBehaviour 基础的服务提供者只能挂载在GameObject上.
+            if (component != null && !component)
+            {
+                throw new LogicException(
+                    "Service providers inherited from MonoBehaviour only be registered mounting on the GameObject.");
+            }
+
+            base.Register(provider, force);
+        }
+
+        protected override bool IsUnableType(Type type)
+        {
+            return typeof(Component).IsAssignableFrom(type) || base.IsUnableType(type);
+        }
+    }
+}
