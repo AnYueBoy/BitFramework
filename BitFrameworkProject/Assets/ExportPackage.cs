ff0a11f51e0c86fa470e59a8b28ea355092330ed
anyue <1183767577@qq.com>
2022-09-28 17:15:43 +0800

    添加框架包导出逻辑

---
+++ BitFrameworkProject/Assets/ExportPackage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportPackage
+{
+    [MenuItem("BitFramework/ExportFramework %e")]
+    private static void ExportFramework()
+    {
+        // 时间戳
+        string timeStamp = DateTime.Now.ToString ("yyyyMMdd");
+        string projectPath = Directory.GetParent (Application.dataPath).FullName;
+        string packagePath = Directory.GetParent (projectPath).FullName;
+
+        string filePathName = packagePath + @"\BitFramework" + ".unitypackage";
+
+        Debug.LogWarning ("path: " + filePathName);
+
+        string assetPathName = "Assets/BitFramework";
+
+        // you can use this api let file name to copy board
+        // GUIUtility.systemCopyBuffer = fileTime;
+
+        AssetDatabase.ExportPackage (assetPathName, filePathName, ExportPackageOptions.Recurse);
+
+        // open package floder
+        Application.OpenURL ("file:///" + packagePath);
+    }
+}
